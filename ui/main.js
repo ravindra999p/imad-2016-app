a5691079a645165cf15d8ff9c06aa60a2d789139
ravindra999p <ravindra999p@gmail.com>
2016-09-21 11:51:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,13 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='new value';
-
+var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+5;
+    img.style.marginLeft=marginLeft+'px';
+}
+img.onclick=function(){
+    var intervel=setIntervel(moveRight,50);
+    
+}
