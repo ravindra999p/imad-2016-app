6b40ab3a1cb63070a22a80254d5a52fd615533ae
ravindra999p <ravindra999p@gmail.com>
2016-09-23 21:20:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,7 +16,7 @@ button.onclick =function () {
 };
 var nameinput=document.getElementById('name')
 var name= nameInput.value;
-var submit=document.getElementById'('submit_btn');
+var submit=document.getElementById'('submit_button');
 submit.onclick=function() {
     var names=['name1','name2'];
     var list='';
