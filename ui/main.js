0af352bec1124c141083eb15e380e7a209616cb1
ravindra999p <ravindra999p@gmail.com>
2016-09-23 21:17:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,31 @@
 console.log('Loaded!');
+var button=document.getElementById('counter');
+button.onclick =function () {
+    var request = new HMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readystate===HMLHttpRequest.DONE) {
+            if (request.status===200){
+                var counter =request.responsetext;
+                var span =document.getElementById('count');
+                span.innerHTML =counter.tostring();
+            }
+        }
+    };
+    request.open('GET','http://ravindra999p.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
+var nameinput=document.getElementById('name')
+var name= nameInput.value;
+var submit=document.getElementById'('submit_btn');
+submit.onclick=function() {
+    var names=['name1','name2'];
+    var list='';
+    for(var i=0;i<names.length;i++) {
+        list+- '<li>' names[i]+</li>;
+)
+var ul = docment.getElementBYId('name list');
+ul.innerHTML= list;
+};
 
 var img=document.getElementById('madi');
 var marginLeft=0;
