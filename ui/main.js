bd60c0373824edecaa70a7296ea6809e58fe9c31
ravindra999p <ravindra999p@gmail.com>
2016-09-27 13:20:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,13 @@
 console.log('Loaded!');
 var button=document.getElementById('counter');
 button.onclick =function () {
-    var request = new HMLHttpRequest();
+    var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (request.readystate===HMLHttpRequest.DONE) {
             if (request.status===200){
-                var counter =request.responsetext;
+                var counter =request.responseText;
                 var span =document.getElementById('count');
-                span.innerHTML =counter.tostring();
+                span.innerHTML =counter.toString();
             }
         }
     };
