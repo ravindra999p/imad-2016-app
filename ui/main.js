4c3517a43cb93dae04577377dc117bb008143eef
ravindra999p <ravindra999p@gmail.com>
2016-09-27 13:22:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,39 +1,62 @@
 console.log('Loaded!');
-var button=document.getElementById('counter');
-button.onclick =function () {
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (request.readystate===HMLHttpRequest.DONE) {
-            if (request.status===200){
-                var counter =request.responseText;
-                var span =document.getElementById('count');
-                span.innerHTML =counter.toString();
-            }
-        }
-    };
+// counter code
+var counter= 0;
+var button= document.getElementById('counter');
+button.onclick= function () {
+    // create a request object
+    var request= new XMLHttpRequest();
+    // capture the response and store it in a variable
+     request.onreadystatechange= function(){
+         if(request.readyState===XMLHttpRequest.DONE){
+             //Take some action
+             if(request.status===200){
+                 var counter= request.responseText;
+                  var span= document.getElementById('count');
+    span.innerHTML=counter.toString();
+             }
+         }
+     };
+
+    //make a request
     request.open('GET','http://ravindra999p.imad.hasura-app.io/counter',true);
     request.send(null);
 };
-var nameinput=document.getElementById('name')
-var name= nameinput.value;
-var submit=document.getElementById('submit_btn');
-submit.onclick= function() {
-    var names=['name1','name2'];
-    var list='';
-    for(var i=0;i<names.length;i++) {
-        list+= '<li>'+ names[i]+ '</li>';
-}
-var ul = document.getElementById('name list');
-ul.innerHTML= list;
-};
+// Submit name
 
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function(){
-    var interval=setInterval(moveRight,50);
-    
-}
+var submit= document.getElementById('submit_btn');
+
+submit.onclick = function () {
+
+  // create a request object
+  var request= new XMLHttpRequest();
+  // capture the response and store it in a variable
+   request.onreadystatechange= function(){
+       if(request.readyState===XMLHttpRequest.DONE){
+           //Take some action
+           if(request.status===200){
+             // capture the list of the names and render it as a list
+                 var names= request.responseText;
+                 names= JSON.parse(names);
+                 var list='';
+                 for (var i=0; i< names.length; i++) {
+
+                     list += '<li>' + names[i] + '</li>';
+
+                 }
+                 var ul= document.getElementById('namelist');
+                 ul.innerHTML= list;
+           }
+       }
+   };
+
+
+   var nameInput= document.getElementById('name');
+   var name= nameInput.value;
+  //make a request
+  request.open('GET','http://ravindra999p.imad.hasura-app.io/submit-name?name='+ name,true);
+  request.send(null);
+
+
+
+
+};
